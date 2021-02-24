fa696d46f880425e806a78631afc79b5d2e0083b
Pathetic Geek <11344611+patheticGeek@users.noreply.github.com>
2021-02-25 03:46:02 +0530

    Firebase config updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,17 @@ import "@firebase/database";
 import "@firebase/analytics";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyB71o1XhMy-0TTHTh1CfbJabPCowI4_au0",
-  authDomain: "coolors-3f8ea.firebaseapp.com",
-  projectId: "coolors-3f8ea",
-  storageBucket: "coolors-3f8ea.appspot.com",
-  messagingSenderId: "957820641224",
-  appId: "1:957820641224:web:1384a8c982e62c0fd72afd",
-  measurementId: "G-ECCR1DD4W9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
-// app.analytics();
+
+app.analytics();
 
 export const auth = app.auth();
 export const database = app.database();
